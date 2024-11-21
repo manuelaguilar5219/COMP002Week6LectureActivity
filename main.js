29da7db1fc84ce1cdcbb07282369faaedc6f19c0
manuelaguilar5219 <manuelaguilar5219@my.whccd.edu>
2024-11-20 20:38:20 -0800

    fixed error

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ let textInput = document.getElementById("textarea"); // Get's element by the ID
 let form = document.querySelector("form"); // "<form>"
 let color = document.getElementById("favcolor");
 
-const storedColor = localStorage.getElementById('favoriteColor');
+const storedColor = localStorage.getElementById("favoriteColor");
 
 if (storedColor) {
     console,log("stored Color: %{storedColor");
@@ -12,12 +12,12 @@ if (storedColor) {
 
 form.addEventListener("submit", event => {
      event.preventDefault();
-    textInput.value="Hello World!"; // Inputs text inside the text area
+    textInput.value="Hello World!"; 
     console.log("Hello World!");
 
     const selectedColor = event.target.value;
 
-    console.log("Selected color: $(selectedColor")
+    console.log("Selected color: $(selectedColor");
 
     localStorage.setItem("favoriteColor", selectedColor);
 });
